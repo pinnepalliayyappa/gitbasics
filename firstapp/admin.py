ccbbfd562cea2738c3f8d162e165def8cee1cd4c
pinnepalliayyappa <68156666+pinnepalliayyappa@users.noreply.github.com>
2021-06-27 11:30:16 +0530

    initial commit

---
+++ firstapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from firstapp.models import employee
+from firstapp.models import register
+# Register your models here.
+admin.site.register(employee)
+admin.site.register(register)
